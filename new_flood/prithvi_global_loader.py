7475e1aff9f2030f5c45fdac11b080874144bb71
Rinki Ghosal <rinki447@gmail.com>
2024-08-27 09:55:42 -0500

    changed some formatting

--- new_flood/prithvi_global_loader.py
+++ new_flood/prithvi_global_loader.py
@@ -15,12 +15,15 @@ from functools import partial
 
 
 class prithvi(nn.Module):
+
     def __init__(self,prithvi_weight,prithvi_config,n_frame,input_size):
+
         super(prithvi,self).__init__()
 
+        # load checkpoint for Prithvi_global
+        
         self.weights_path =prithvi_weight
         self.checkpoint = torch.load(self.weights_path)
-
         config = prithvi_config
 
         
@@ -41,35 +44,16 @@ class prithvi(nn.Module):
                                  coords_scale_learn=config.MODEL.COORDS_SCALE_LEARN,
                                  drop_channels_rate=config.MODEL.DROP_CHANNELS_RATE)
 
-        self.prithvi_model.eval()
+        self.prithvi_model.eval() # to double check the prithvi weights being frozen
         print("checkpoint names:",self.checkpoint.keys())
          
         _ = self.prithvi_model.load_state_dict(self.checkpoint, strict=False)
-        #print("initialized model")
         
+    
     def forward(self,x,temp,loc,mask):
-        #print("forward function")
+
         out,_,_=self.prithvi_model.forward_encoder(x,temp,loc,mask) 
         return out
 
-'''
-#initialize model    
-pr_weight="/rhome/rghosal/Rinki/rinki-hls-foundation-os/Prithvi_global.pt"
 
-pr_config= get_config(None)  
-print("model:",pr_config)
-
-n_frame=3
-model_prithvi=prithvi(pr_weight,pr_config,n_frame)
-
-#check blocks of the model after loading its weights
-######print("loaded model blocks",model_pvi.pvi_model)
-
-input_tensor = torch.randn(1, 6, 3, 224, 224)  # Input batch,channel,frame,height,width
-temp_coord=None #torch.randn((1,366),1, 3,2)
-loc_coord=None #torch.randn((0,90),1, 2)
-with torch.no_grad():
-    prithvi_loaded=model_prithvi(input_tensor,temp_coord,loc_coord,0)
-
-print("prithvi forward passed")'''
 
