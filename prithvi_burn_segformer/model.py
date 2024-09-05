132396e50d9cc869c4703f050de65b52d66488b1
RINKI GHOSAL <132046732+rinki447@users.noreply.github.com>
2024-09-05 11:29:47 -0500

    Update model.py

--- prithvi_burn_segformer/model.py
+++ prithvi_burn_segformer/model.py
@@ -10,6 +10,8 @@ class prithvi_wrapper(nn.Module):
     def __init__(self, segmentor_config,n_channels, n_classes,n_frame,embed_size,input_size,patch_size,prithvi_weight,prithvi_config):
         super(prithvi_wrapper, self).__init__()
 
+        #variables for prithvi_global MAE
+        
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.pr_weight=prithvi_weight
@@ -18,11 +20,14 @@ class prithvi_wrapper(nn.Module):
         self.input_size=input_size
         self.embed_size=embed_size
         self.patch_size=patch_size
-        self.seg_conf=segmentor_config
+        
  
         self.prithvi=prithvi(self.pr_weight,self.pr_config,n_frame,input_size)
         #print("initialize prithvi")
-
+        
+        # variables for the segmentor head
+        
+        self.seg_conf=segmentor_config
         n_layers=self.seg_conf["n_layers"]
         n_heads=self.seg_conf["n_heads"]
         d_model=self.seg_conf["decoder_embed_dim"]
