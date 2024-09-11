b7b84c83464d7997316b28432baa53529f3e4527
Rinki Ghosal <rinki447@gmail.com>
2024-09-11 11:54:34 -0500

    deleted old folder

--- prithvi_burn_intensity/test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-weights_path ="/rhome/rghosal/Rinki/rinki-hls-foundation-os/flood_checkpoint/checkpoint_flood_new_sample.pth" 
-
-checkpoint = torch.load(weights_path)
-
-print(checkpoint.keys())
