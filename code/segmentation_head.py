639fb94024c8667fe72cf25c32c1d053b97634c4
xhagrg <grg.iksha@hotmail.com>
2024-11-07 15:52:41 -0600

    Update segmentation head.

--- code/segmentation_head.py
+++ code/segmentation_head.py
@@ -1,38 +1,42 @@
-from torch.nn import nn
-from lib.model_utils import upscaling_block, upscaling_block2
+import torch
+import torch.nn as nn
 
-class SegmentationHead(nn.Module):
-    def __init__(self,embed_size,n_classes,n_frame,input_size,patch_size):
-        super(SegmentationHead, self).__init__()
-
-        self.image_size = input_size
-        self.grid_dim = input_size[-1]//patch_size[-1]
-        self.embed_dim = embed_size
-        self.n_frame = n_frame
-
-        # Conv layer to upscale the token grid to the desired segmented image size
-        self.up1 = upscaling_block(self.embed_dim,256)
-        self.up2 = upscaling_block(256,128)
-        self.up3 = upscaling_block(128,64)
-        self.up4 = upscaling_block(64,32)
-        self.up5 = upscaling_block2(32,n_classes)
+class ConvModule(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(ConvModule, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.activate = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        batch_size = x.size(0)
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activate(x)
+        return x
 
-        # Reshape x to (batch, embed_size, grid_dim, grid_dim)
+# Define FCNHead
+class FCNHead(nn.Module):
+    def __init__(self, in_channels, channels, num_classes, dropout_p=0.1):
+        super(FCNHead, self).__init__()
 
-        x = x.reshape(batch_size,self.embed_dim,self.grid_dim, self.grid_dim)
-        #print("x shape:",x.shape)
-        x = self.up1(x)
-        #print("x shape:",x.shape)
-        x = self.up2(x)
-        #print("x shape:",x.shape)
-        x = self.up3(x)
-        #print("x shape:",x.shape)
-        x = self.up4(x)
-        #print("x shape:",x.shape)
-        x=self.up5(x)
-        #print("x shape:",x.shape)
+        #self.loss_decode = DiceLoss()  # Dice loss
 
+        self.convs = nn.Sequential(
+            ConvModule(in_channels, channels, kernel_size=3, stride=1, padding=1)
+        )
+        self.dropout = nn.Dropout2d(p=dropout_p)
+        self.conv_seg = nn.Conv2d(channels, num_classes, kernel_size=1)  # Conv2d for segmentation
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # Initialize conv_seg weights with Normal distribution (std=0.01)
+        nn.init.normal_(self.conv_seg.weight, std=0.01)
+        if self.conv_seg.bias is not None:
+            nn.init.constant_(self.conv_seg.bias, 0)
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = self.dropout(x)
+        x = self.conv_seg(x)  # Final segmentation output
         return x
