5f8e535cb780b6d74b8359264aee5c3e18992ce7
xhagrg <grg.iksha@hotmail.com>
2024-11-08 10:30:55 -0600

    Use method instead of just running the script.

--- code/train_burn_scars.py
+++ code/train_burn_scars.py
@@ -1,4 +1,5 @@
 from lib.trainer import Trainer
 
-trainer = Trainer('../config/burn_scars.yaml')
-trainer.train()
+def train():
+    trainer = Trainer('../config/burn_scars.yaml')
+    trainer.train()
