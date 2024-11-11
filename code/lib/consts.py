369bb0b5f05122d09d5a6da2b867ae40dd5e66eb
xhagrg <grg.iksha@hotmail.com>
2024-11-11 08:48:55 -0600

    Add constants.

--- code/lib/consts.py
+++ code/lib/consts.py
@@ -1,6 +1,11 @@
+import os
+
 NO_DATA = -9999
 NO_DATA_FLOAT = 0.0001
 PERCENTILES = (0.1, 99.9)
 
 CROP_SIZE = (224, 224)
 SPLITS = ['training', 'validation', 'test']
+
+BUCKET_NAME = os.environ['BUCKET_NAME']
+MODEL_PATH = "models/{model_name}"
